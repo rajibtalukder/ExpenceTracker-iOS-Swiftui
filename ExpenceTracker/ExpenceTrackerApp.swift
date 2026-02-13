560c759781462254c549a9f58637451394e58ac4
rajibtalukder <rajibtalukder392@gmail.com>
2026-02-13 11:30:19 +0600

    Initial Commit

---
+++ ExpenceTracker/ExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenceTrackerApp.swift
+//  ExpenceTracker
+//
+//  Created by MacBook on 13/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
