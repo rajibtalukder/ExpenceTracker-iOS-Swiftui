be94c542b3340ffc13e7a9707aa6cfbfa9db5a52
rajibtalukder <rajibtalukder392@gmail.com>
2026-02-13 12:26:38 +0600

    previous changes

--- ExpenceTracker/ContentView.swift
+++ ExpenceTracker/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ScrollView{}
+                .background(Color(Color.background))
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem{
+                        Image(systemName: "bell.badge")
+                    }
+                }
+        }.navigationViewStyle(.stack)
     }
 }
 
